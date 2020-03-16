c258cbc4d55aa20ba6e401192aee9aff41368266
Aman Kansal <amankansal.india@gmail.com>
2020-03-16 15:27:21 +0530

    adds positional arg on_delete

--- pool/models.py
+++ pool/models.py
@@ -16,15 +16,15 @@ class User(models.Model):
 	                              related_name="members")
 	
 class GroupMember(models.Model):
-	group_id=models.ForeignKey(Group,models.DO_NOTHING)
-	user_id=models.ForeignKey(User,models.DO_NOTHING)
+	group_id=models.ForeignKey(Group,on_delete=models.DO_NOTHING)
+	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
 	
 	
 class Service(PolymorphicModel):
 	group_ids=models.CharField(validators=[validate_comma_separated_integer_list],
 	                                  max_length=200, blank=True, null=True,default='')
-	category_id=models.ForeignKey(Category,models.DO_NOTHING,related_name="%(class)s_services")
-	initiator_id=models.ForeignKey(User,models.DO_NOTHING)
+	category_id=models.ForeignKey(Category,on_delete=models.DO_NOTHING,related_name="%(class)s_services")
+	initiator_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
 	start_time=models.DateTimeField(null=True)
 	end_time=models.DateTimeField(null=True)
 	slackness=models.DurationField()
@@ -48,13 +48,13 @@ class ShoppingService(Service):
 	vendor=models.CharField(null=False,max_length=1000)
 
 class ServiceMember(models.Model):
-	service_id=models.ForeignKey(Service,models.DO_NOTHING)
-	user_id=models.ForeignKey(User,models.DO_NOTHING)
+	service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING)
+	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
 
 class Message(models.Model):
 	timestamp=models.DateTimeField(null=False,auto_now=True)
 	content=models.CharField(default='',max_length=1000)
-	service_id=models.ForeignKey(Service,models.DO_NOTHING,related_name="messages")
-	user_id=models.ForeignKey(User,models.DO_NOTHING,related_name="messages")
+	service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING,related_name="messages")
+	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="messages")
 	
 # Create your models here.
