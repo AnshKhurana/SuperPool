17dc679a805022194d9fd3fc3c8075bc326a0ef7
AnshKhurana <anshkhurana1507@gmail.com>
2020-03-16 19:14:23 +0530

    Adds api key

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+gmaps_api_key = "AIzaSyAEdRHs5qFXBpXu3HkQXcRXnwBzSbiYZv4"
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
@@ -16,7 +18,10 @@ class User(AbstractUser):
         error_messages={
             'unique': "A user with that username already exists.",
         },
-    )
+    ) #
+
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
 
     email = models.EmailField(unique=True, blank=False,
                               error_messages={
@@ -24,14 +29,18 @@ class User(AbstractUser):
                               })
 
     phone_number = PhoneNumberField(null=False, blank=False, unique=True, error_messages=dict(
-        unique="A user with that email already exists."))
+        unique="A user with that phone number already exists."))
 
-    gender = models.CharField(max_length=20)
-
-    # Status for terms and conditions
-    tc_status = models.BooleanField(default=False)
-
+    # any string description
     about = models.TextField(blank=True)
+
+    # location specs
+
+
+
+
+    # # Status for terms and conditions, enforce in HTML
+    # tc_status = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "gender"]
--- pool/models.py
+++ pool/models.py
@@ -3,58 +3,58 @@ from django.core.validators import validate_comma_separated_integer_list
 from polymorphic.models import PolymorphicModel
 
 class Category(models.Model):
-	name=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
 
 class User(models.Model):
-	name=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
 
 class Group(models.Model):
-	name=models.CharField(max_length=255)
-	members=models.ManyToManyField(User,
-	                               through="GroupMember",
-	                               through_fields=("group_id","user_id"),
-	                               related_name="groups")
-	
+    name=models.CharField(max_length=255)
+    members=models.ManyToManyField(User,
+                                   through="GroupMember",
+                                   through_fields=("group_id","user_id"),
+                                   related_name="groups")
+
 class GroupMember(models.Model):
-	group_id=models.ForeignKey(Group,on_delete=models.DO_NOTHING)
-	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
-	
-	
+    group_id=models.ForeignKey(Group,on_delete=models.DO_NOTHING)
+    user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+
+
 class Service(PolymorphicModel):
-	group_ids=models.CharField(validators=[validate_comma_separated_integer_list],
-	                                  max_length=200, blank=True, null=True,default='')
-	category_id=models.ForeignKey(Category,on_delete=models.DO_NOTHING,related_name="%(class)s_services")
-	initiator_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
-	start_time=models.DateTimeField(null=True)
-	end_time=models.DateTimeField(null=True)
-	slackness=models.DurationField()
-	description=models.CharField(null=False,max_length=1000) #this is a general description
-	stype=models.CharField(null=False,max_length=255) # one among ['Food','Travel','Shopping']
-	members=models.ManyToManyField(User,
-	                               through="ServiceMember",
-	                               through_fields=("service_id","user_id"),
-	                               related_name="%(class)s_services")
-	
-		
-	
+    group_ids=models.CharField(validators=[validate_comma_separated_integer_list],
+                               max_length=200, blank=True, null=True,default='')
+    category_id=models.ForeignKey(Category,on_delete=models.DO_NOTHING,related_name="%(class)s_services")
+    initiator_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    start_time=models.DateTimeField(null=True)
+    end_time=models.DateTimeField(null=True)
+    slackness=models.DurationField()
+    description=models.CharField(null=False,max_length=1000) #this is a general description
+    stype=models.CharField(null=False,max_length=255) # one among ['Food','Travel','Shopping']
+    members=models.ManyToManyField(User,
+                                   through="ServiceMember",
+                                   through_fields=("service_id","user_id"),
+                                   related_name="%(class)s_services")
+
+
+
 class TravelService(Service):
-	start_point=models.CharField(null=False,max_length=1000)
-	end_point=models.CharField(null=False,max_length=1000)
+    start_point=models.CharField(null=False,max_length=1000)
+    end_point=models.CharField(null=False,max_length=1000)
 
 class FoodService(Service):
-	vendor=models.CharField(null=False,max_length=1000)
+    vendor=models.CharField(null=False,max_length=1000)
 
 class ShoppingService(Service):
-	vendor=models.CharField(null=False,max_length=1000)
+    vendor=models.CharField(null=False,max_length=1000)
 
 class ServiceMember(models.Model):
-	service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING)
-	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING)
+    user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
 
 class Message(models.Model):
-	timestamp=models.DateTimeField(null=False,auto_now=True)
-	content=models.CharField(default='',max_length=1000)
-	service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING,related_name="messages")
-	user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="messages")
-	
+    timestamp=models.DateTimeField(null=False,auto_now=True)
+    content=models.CharField(default='',max_length=1000)
+    service_id=models.ForeignKey(Service,on_delete=models.DO_NOTHING,related_name="messages")
+    user_id=models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="messages")
+
 # Create your models here.
