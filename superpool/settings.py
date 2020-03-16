1fd6809bdcc21a372b83edb615ed5cf5bb708a45
pranayreddys <pranayr@cse.iitb.ac.in>
2020-03-16 22:43:49 +0530

    Latest

--- superpool/settings.py
+++ superpool/settings.py
@@ -129,7 +129,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 AUTH_USER_MODEL = "accounts.User"
