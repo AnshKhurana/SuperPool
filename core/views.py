c9c12f5578bb95e261bb16c674bb3ebb253d4610
Aman Kansal <amankansal.india@gmail.com>
2020-03-17 19:40:56 +0530

    makes login page showup

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.db.models import Q
+
+
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse_lazy('accounts:login'))
+	
+	
+    return render(request, 'home.html')
