ad5f31161a6c6e188686628cf77b0dd07e6a50a9
AnshKhurana <anshkhurana1507@gmail.com>
2020-03-18 23:35:50 +0530

    removed redundant app

--- superpool/urls.py
+++ superpool/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('',include('core.urls')),
+    path('',include('pool.urls')),
     path('groups/',include('groups.urls'))
 ]
