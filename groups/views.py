95182e44d02de5021c103f053b8fd00ab24ff2a7
kansalaman <amankansal.india@gmail.com>
2020-03-18 11:59:54 +0530

    partial progress in groups

---
+++ groups/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from pool.models import *
+
+
+class grouphome(ListView):
+    # model = GroupMember
+    context_object_name = 'groups'
+    template_name = 'groups/groups.html'
+
+    def get_queryset(self):
+        # current_user_friends = self.request.user.friends.values('id')
+        # sent_request = list(Friend.objects.filter(user=self.request.user).values_list('friend_id', flat=True))
+        # users = User.objects.exclude(id__in=current_user_friends).exclude(id__in=sent_request).exclude(id=self.request.user.id)
+        groups=self.request.user.groups.values_list()
+        return groups
--- superpool/settings.py
+++ superpool/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pool',
-    'accounts'
+    'accounts',
+    'core',
+    'groups'
 ]
 
 MIDDLEWARE = [
