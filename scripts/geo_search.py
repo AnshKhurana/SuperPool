c6a25cc076e0067dd93e8095b8eeebdfc876d187
Aman Kansal <amankansal.india@gmail.com>
2020-03-24 17:19:10 +0530

    adds scripts for location suggestion and restaurants

---
+++ scripts/geo_search.py
@@ -0,0 +1,16 @@
+import requests
+import json
+api_key = 'J6updMEyBoTKrXSBqhOpjO1sP64AjPPB2FwKPwJUNfU'
+# raw_query='''https: // autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json?apiKey = {{{}}}&query=Pariser+1+Berl&beginHighlight = <b >&endHighlight = </b >'''.format(api_key).replace(' ','')
+raw_query = '''https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json'''
+print(raw_query)
+response=requests.get(raw_query,params={
+    'apiKey': api_key,
+    'query':'iit bombay'
+}).json()
+
+
+
+print(json.dumps(response,indent=2))
+
+
---
+++ scripts/get_restaurant_data.sh
@@ -0,0 +1,4 @@
+# kaggle datasets download -d shrutimehta/zomato-restaurants-data
+kaggle datasets download -d srivpuneet16/zomato-mumbai-restaurant-analysis
+unzip zomato-mumbai-restaurant-analysis.zip
+
