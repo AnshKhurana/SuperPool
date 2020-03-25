84f430d982665aaad1ed4ca1d08fe7631fce6c00
AnshKhurana <anshkhurana1507@gmail.com>
2020-03-26 00:49:32 +0530

    css template

--- api/serializers.py
+++ api/serializers.py
@@ -4,20 +4,20 @@ from rest_framework import serializers
 class FoodServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FoodService
-        fields = ['start_time', 'end_time', 'slackness', 'description', 'vendor']
+        fields = ['start_time', 'end_time', 'initiator', 'description', 'vendor']
 
 class TravelServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TravelService
-        fields = ['start_time', 'end_time', 'slackness', 'description', 'start_point', 'end_point']
+        fields = ['start_time', 'end_time', 'initiator', 'description', 'start_point', 'end_point']
 
 class ShoppingServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ShoppingService
-        fields = ['start_time', 'end_time', 'slackness', 'description', 'vendor']
+        fields = ['start_time', 'end_time', 'initiator', 'description', 'vendor']
 
 
-class ServiceSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ShoppingService
-        fields = ['category','initiator', 'start_time', 'end_time', 'slackness', 'description']
+# class ServiceSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = ShoppingService
+#         fields = ['category','initiator', 'start_time', 'end_time', 'slackness', 'description']
