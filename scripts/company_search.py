54c541884b8af78ff0730209f0270bf616fb94c7
kansalaman <amankansal.india@gmail.com>
2020-03-26 16:42:19 +0530

    adds company api

---
+++ scripts/company_search.py
@@ -0,0 +1,6 @@
+import json
+import requests
+
+company_api_url="https://autocomplete.clearbit.com/v1/companies/suggest"
+response=requests.get(company_api_url,params={'query':input("Enter company name: ")}).json()
+print(json.dumps(response,indent=2))
