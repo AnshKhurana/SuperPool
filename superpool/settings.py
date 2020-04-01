934a4f3c2fe583d45cf1280d90c8f3170b241050
pranayreddys <pranayr@cse.iitb.ac.in>
2020-04-01 18:08:47 +0530

    Fixed bug in REDIS settings.py

--- superpool/settings.py
+++ superpool/settings.py
@@ -14,7 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-redis_host = os.environ.get('REDIS_HOST', '192.168.43.108')
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
