7124fdae64b215254c39d001de99494b5d716517
Arnabjana1999 <ajana47@gmail.com>
2020-04-02 11:55:21 +0530

    sidebar integrated in My Services and made the default homepage

--- api/views.py
+++ api/views.py
@@ -4,45 +4,45 @@ from .serializers import *
 from pool.models import *
 from django.db.models import Q
 
+
 # Create your views here.
 class FoodServiceList(generics.ListAPIView):
-
     serializer_class = FoodServiceSerializer
+
     def get_queryset(self):
         user = self.request.user
         gids = self.request.GET.get('gids').split(',')
-        filt= Q(groups__id__in=gids) & Q(category__name='Food') & Q(groups__members=user.id)
+        filt = Q(groups__id__in=gids) & Q(category__name='Food') & Q(groups__members=user.id)
         if 'start' in self.request.GET and 'end' in self.request.GET:
-            start= self.request.GET.get('start')
-            end= self.request.GET.get('end')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
             filt = filt & Q(start_time__range=(start, end))
         return Service.objects.filter(filt).all()
 
-class TravelServiceList(generics.ListAPIView):
 
+class TravelServiceList(generics.ListAPIView):
     serializer_class = TravelServiceSerializer
 
     def get_queryset(self):
         user = self.request.user
         gids = self.request.GET.get('gids').split(',')
-        filt= Q(groups__id__in=gids) & Q(category__name='Travel') & Q(groups__members=user.id)
+        filt = Q(groups__id__in=gids) & Q(category__name='Travel') & Q(groups__members=user.id)
         if 'start' in self.request.GET and 'end' in self.request.GET:
-            start= self.request.GET.get('start')
-            end= self.request.GET.get('end')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
             filt = filt & Q(start_time__range=(start, end))
         return Service.objects.filter(filt).all()
 
-class ShoppingServiceList(generics.ListAPIView):
 
+class ShoppingServiceList(generics.ListAPIView):
     serializer_class = ShoppingServiceSerializer
 
     def get_queryset(self):
         user = self.request.user
         gids = self.request.GET.get('gids').split(',')
-        filt= Q(groups__id__in=gids) & Q(category__name='Shopping') & Q(groups__members=user.id)
+        filt = Q(groups__id__in=gids) & Q(category__name='Shopping') & Q(groups__members=user.id)
         if 'start' in self.request.GET and 'end' in self.request.GET:
-            start= self.request.GET.get('start')
-            end= self.request.GET.get('end')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
             filt = filt & Q(start_time__range=(start, end))
         return Service.objects.filter(filt).all()
-
