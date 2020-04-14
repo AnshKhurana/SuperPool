d57a2a3d6e6bf5766f7cfba42f841589c3a3ad05
Arnabjana1999 <ajana47@gmail.com>
2020-04-14 11:47:15 +0530

    Minor modifications made

--- onetime.sh
+++ onetime.sh
@@ -1,7 +1,7 @@
 mkdir server
 cd server
-initdb -D pool -U superpool 
-pg_ctl -D pool -l logfile start
+initdb -D pool -U superpool #/usr/lib/postgresql/10/bin/initdb -D pool -U superpool
+pg_ctl -D pool -l logfile start #/usr/lib/postgresql/10/bin/pg_ctl -D pool -l logfile start
 # edit port number to 9001
 # use  psql -h localhost -p 9001  -U superpool -d postgres
 # CREATE DATABASE pool;
